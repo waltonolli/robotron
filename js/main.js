40d35c37353ca6b603e5580756af0c863057a94e
Walkyria <137917056+waltonolli@users.noreply.github.com>
2023-08-05 23:47:56 +0200

    Completed course project Robotron Alura

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// const subtrair = document.querySelector("#subtrair")
+// const somar = document.querySelector("#somar")
+
+
+const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+
+//objetos 
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach ( (element) => {
+    element.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+        atualizaEstatisticas(evento.target.dataset.peca);
+    })    
+})
+
+// subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+
+// somar.addEventListener("click", () => {manipulaDados("somar")})
+
+function manipulaDados (operacao, controle) {
+    const peca = controle.querySelector("[data-contador]");
+
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    } else {
+        peca.value = parseInt(peca.value) + 1;
+    }
+} 
+
+function atualizaEstatisticas(peca) {
+    console.log(pecas[peca]);
+
+    estatisticas.forEach( (element) => {
+        element.textContent = parseInt(element.textContent) + pecas[peca] [element.dataset.estatistica];      
+    })
+}
+
+function trocaImagem(cor){
+    document.querySelector(".robo").src="img/Robotron 2000 - " + cor + ".png";
+ }
